01826ea6c0a838f4be86ba91ad1a91ad5de8db16
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-12-12 09:52:44 +0700

    Initial commit

---
+++ src/test/java/com/bayu/web/WebControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
