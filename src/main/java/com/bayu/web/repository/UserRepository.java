ef83d9bf918c97db6325b5a5a3890986c3accf77
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-12-12 10:16:55 +0700

    add repository UserRepository

---
+++ src/main/java/com/bayu/web/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bayu.web.repository;
+
+import com.bayu.web.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
